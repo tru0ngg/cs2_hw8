5850a6fbdb5d7d7d702fa1c421ac1c6cc2356595
tru0ngg <dangtru@bc.edu>
2025-11-15 14:34:02 -0500

    Rectangle.cpp

--- Rectangle.cpp
+++ Rectangle.cpp
@@ -7,7 +7,7 @@ Rectangle::Rectangle(int row, int column, int width, int height, char marker) : 
 void Rectangle::blit(char canvas[20][40]){
         for (int r = its_row; r < its_row + its_height; ++r){
                 for (int c = its_column; c < its_column + its_width; ++c){
-                        if (r >= 0 && r <= 20 && c >= 0 && c <= 40){
+                        if (r >= 0 && r < 20 && c >= 0 && c < 40){
                                 canvas[r][c] = its_marker;
                         }
                 }
