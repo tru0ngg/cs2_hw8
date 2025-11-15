4e994070ec32d159bcfc672bbe482ccdc3e8fafe
tru0ngg <dangtru@bc.edu>
2025-11-15 14:36:47 -0500

    Outline.cpp

--- Outline.cpp
+++ Outline.cpp
@@ -8,7 +8,7 @@ void Outline::blit(char canvas[20][40]){
                 for (int c = its_column; c < its_column + its_width; c++){
                         bool not_filled = ( r == its_row || r == its_row + its_height - 1 || c == its_column || c == its_column + its_width - 1);
                         if (not_filled){
-                                if (r >= 0 && r <= 20 && c >= 0 && c <= 40){
+                                if (r >= 0 && r < 20 && c >= 0 && c < 40){
                                         canvas[r][c] = its_marker;
                                 }
                         }
